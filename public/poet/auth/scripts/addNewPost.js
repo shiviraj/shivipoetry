bed3a9c91c85b56bbd83470bc768b0971bb534ea
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-05-25 13:47:02 +0530

    |#6|Shiviraj|Added style in addPost page

--- public/poet/auth/scripts/addNewPost.js
+++ public/poet/auth/scripts/addNewPost.js
@@ -1,13 +1,13 @@
 const renderCategories = (categories) => {
   const categoryCheckbox = categories.map((category) => {
-    return `<input type="checkbox" name="${category.url}" value="${category.url}">${category.name}`;
+    return `<label><input type="checkbox" name="${category.url}" value="${category.url}">${category.name}</label>`;
   });
   getElement('#category-list').innerHTML = categoryCheckbox.join('');
 };
 
 const renderTags = (tags) => {
   const tagCheckbox = tags.map((tag) => {
-    return `<input type="checkbox" name="${tag.url}" value="${tag.url}">${tag.name}`;
+    return `<label><input type="checkbox" name="${tag.url}" value="${tag.url}">${tag.name}</label>`;
   });
   getElement('#tag-list').innerHTML = tagCheckbox.join('');
 };

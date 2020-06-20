3a3d6e65981eff9f2921ef932aedc920e2e14069
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-06-20 23:22:06 +0530

    |#2|Shiviraj|fixed a minor bug in to show post

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
-const {Author} = require('../models/author');
+const { Author } = require('../models/author');
 
 const auth = async function (req, res, next) {
   try {
     const token = req.cookies.token.replace('token ', '');
-    const {_id} = jwt.verify(token, process.env.SECRET_CODE);
-    const author = await Author.findOne({_id, 'tokens.token': token});
+    const { _id } = jwt.verify(token, process.env.SECRET_CODE);
+    const author = await Author.findOne({ _id, 'tokens.token': token });
     if (!author) throw new Error();
     req.token = token;
     req.author = author;
@@ -15,4 +15,4 @@ const auth = async function (req, res, next) {
   }
 };
 
-module.exports = {auth};
+module.exports = { auth };
